bb7c0480bac07b98463796c6860a0424c9f48b7c
Kenny Halim <kennyhalim98@gmail.com>
2019-02-22 08:55:17 -0800

    add code for HomeController.cs

---
+++ WordCounter.Tests/ControllerTests/HomeController.Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+    
+}
