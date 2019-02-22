e7ba7fe8b05cffee3abc51274d85d6299d2b4be2
Kenny Halim <kennyhalim98@gmail.com>
2019-02-22 09:18:50 -0800

    add constructor again and fix methods

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -9,120 +9,120 @@ namespace WordCounter.Tests
     [TestMethod]
     public void IsWordAlphabetical_False()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
+      RepeatCounter testRepeatCounter = new RepeatCounter("apple is great!", "wow");
       Assert.AreEqual(false, testRepeatCounter.IsAlphabetical("apple is great!", "wow"));
     }
 
     [TestMethod]
     public void IsWordAlphabetical_True()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am testing this program", "program");
       Assert.AreEqual(true, testRepeatCounter.IsAlphabetical("I am testing this program", "program"));
     }
 
     [TestMethod]
     public void TestOneWordAndOneSentence_False()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("apple", "orange"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("apple", "orange");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void TestOneWordAndOneSentence_True()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(1, testRepeatCounter.Count("apple", "apple"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("apple", "apple");
+      Assert.AreEqual(1, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void IsEatInSentence_False()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("I am am testing this program", "eat"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am am testing this program", "eat");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void IsEatInSentence_True()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(1, testRepeatCounter.Count("I am going to eat after this class", "eat"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am going to eat after this class", "eat");
+      Assert.AreEqual(1, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountTestInSentence()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(3, testRepeatCounter.Count("This is a test to test the test method is working", "test"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("This is a test to test the test method is working", "test");
+      Assert.AreEqual(3, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountTestInInvalidSentence()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("This is a test! to test the test method is working", "test"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("This is a test! to test the test method is working", "test");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountWordNotFullyMatchingCase()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("I am going to the cathedral", "cat"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am going to the cathedral", "cat");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void TestBlankSentenceAndBlankWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
+      RepeatCounter testRepeatCounter = new RepeatCounter("", "");
       Assert.AreEqual(false, testRepeatCounter.IsAlphabetical("", ""));
     }
 
     [TestMethod]
     public void TestBlankWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
+      RepeatCounter testRepeatCounter = new RepeatCounter("This is a sentence", "");
       Assert.AreEqual(false, testRepeatCounter.IsAlphabetical("This is a sentence", ""));
     }
 
     [TestMethod]
     public void TestBlankSentence()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
+      RepeatCounter testRepeatCounter = new RepeatCounter("", "Test");
       Assert.AreEqual(false, testRepeatCounter.IsAlphabetical("", "Test"));
     }
 
     [TestMethod]
     public void CountBlankSentenceAndBlankWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("", ""));
+      RepeatCounter testRepeatCounter = new RepeatCounter("", "");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountBlankWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("This is a sentence", ""));
+      RepeatCounter testRepeatCounter = new RepeatCounter("This is a sentence", "");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountBlankSentence()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(0, testRepeatCounter.Count("", "Test"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("", "Test");
+      Assert.AreEqual(0, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountUppercaseSentenceAndLowerCaseWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(1, testRepeatCounter.Count("I am going to France next year", "france"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am going to France next year", "france");
+      Assert.AreEqual(1, testRepeatCounter.Count());
     }
 
     [TestMethod]
     public void CountLowerCaseSentenceAndUpperCaseWord()
     {
-      RepeatCounter testRepeatCounter = new RepeatCounter();
-      Assert.AreEqual(1, testRepeatCounter.Count("I am going to eat some bananas", "Bananas"));
+      RepeatCounter testRepeatCounter = new RepeatCounter("I am going to eat some bananas", "Bananas");
+      Assert.AreEqual(1, testRepeatCounter.Count());
     }
   }
 }
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -5,12 +5,55 @@ namespace WordCounter
 {
   public class RepeatCounter
   {
+    private string _sentence;
+    private string _word;
+    private static List<RepeatCounter> _List = new List<RepeatCounter> {};
+    private int _id;
+
+    public RepeatCounter (string sentence, string word)
+    {
+      _sentence = sentence;
+      _word = word;
+      _List.Add(this);
+      _id = _List.Count;
+    }
+
+    public string GetSentence()
+    {
+      return _sentence;
+    }
+
+    public string GetWord()
+    {
+      return _word;
+    }
+
+    public static List<RepeatCounter> GetAll()
+    {
+      return _List;
+    }
+
+    public static void ClearAll()
+    {
+      _List.Clear();
+    }
+
+    public static RepeatCounter Find(int searchId)
+    {
+      return _List[searchId-1];
+    }
+
+    public int GetId()
+    {
+     return _id;
+    }
+
     public bool IsAlphabetical(string sentence, string word)
     {
-      if(string.IsNullOrEmpty(sentence) || string.IsNullOrEmpty(word)){
+      if(string.IsNullOrEmpty(_sentence) || string.IsNullOrEmpty(_word)){
         return false;
       }
-      string sentenceWord = sentence + ' ' + word;
+      string sentenceWord = _sentence + ' ' + _word;
       foreach (char c in sentenceWord)
       {
         if (!Char.IsLetterOrDigit(c) && c != ' ') //Only allow alphabets, numbers and space (No Punctuation)
@@ -19,12 +62,12 @@ namespace WordCounter
       return true;
     }
 
-    public int Count(string sentence, string word){
+    public int Count(){
       int counter = 0;
-      string[] wordsInSentence = sentence.Split(' ');
-      if(IsAlphabetical(sentence, word)){
+      string[] wordsInSentence = _sentence.Split(' ');
+      if(IsAlphabetical(_sentence, _word)){
         for(int i = 0; i < wordsInSentence.Length; i++){
-          if(word.ToLower() == wordsInSentence[i].ToLower()){
+          if(_word.ToLower() == wordsInSentence[i].ToLower()){
             counter++;
           }
         }
