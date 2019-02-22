8104a592265ac31de3962dddbce173f4cc26a277
Kenny Halim <kennyhalim98@gmail.com>
2019-02-22 12:25:35 -0800

    edit RepeatCounter.cs file

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -7,15 +7,11 @@ namespace WordCounter
   {
     private string _sentence;
     private string _word;
-    private static List<RepeatCounter> _List = new List<RepeatCounter> {};
-    private int _id;
 
     public RepeatCounter (string sentence, string word)
     {
       _sentence = sentence;
       _word = word;
-      _List.Add(this);
-      _id = _List.Count;
     }
 
     public string GetSentence()
@@ -26,26 +22,6 @@ namespace WordCounter
     public string GetWord()
     {
       return _word;
-    }
-
-    public static List<RepeatCounter> GetAll()
-    {
-      return _List;
-    }
-
-    public static void ClearAll()
-    {
-      _List.Clear();
-    }
-
-    public static RepeatCounter Find(int searchId)
-    {
-      return _List[searchId-1];
-    }
-
-    public int GetId()
-    {
-     return _id;
     }
 
     public bool isEmpty()
